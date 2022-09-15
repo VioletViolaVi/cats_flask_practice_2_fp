92b882c384a86350253782977c9c0cc18827cacc
vivian <vivian.v18723@gmail.com>
2022-09-15 21:37:00 +0100

    complete practice from lesson

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from werkzeug import exceptions
 
 from controllers import cats
 
@@ -20,10 +21,31 @@ def cat():
     #     return "We have 'POST' some cats", 201
     fns = {
         "GET": cats.index,
-        "POST": cats.create
+        "POST": cats.create,
     }
     resp, code = fns[request.method](request)
     return jsonify(resp), code
+
+
+@app.route("/cats/<int:cat_id>", methods=["GET", "PUT", "DELETE"])
+def cat_handler(cat_id):
+    fns = {
+        "GET": cats.show,
+        "PUT": cats.update,
+        "DELETE": cats.destroy
+    }
+    resp, code = fns[request.method](request, cat_id)
+    return jsonify(resp), code
+
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return jsonify({"message": f"Oops... {err}"}), 404
+
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_50(err):
+    return jsonify({"message": f"It's not you, it's us {err}"}), 500
 
 
 if __name__ == "__main__":
--- controllers/cats.py
+++ controllers/cats.py
@@ -1,3 +1,5 @@
+from werkzeug import exceptions
+
 cats = [
     {"id": 1, "name": "Muffy", "age": 9},
     {"id": 2, "name": "Brian", "age": 8},
@@ -15,3 +17,33 @@ def create(req):
     new_cat["id"] = sorted([c["id"] for c in cats])[-1] + 1
     cats.append(new_cat)
     return new_cat, 201
+
+
+def show(req, uid):
+    return find_by_id(uid), 200
+
+
+def update(req, uid):
+    cat = find_by_id(uid)
+    data = req.get_json()
+    for key, val in data.items():
+        cat[key] = val
+    return cat, 200
+
+
+def destroy(req, uid):
+    cat = find_by_id(uid)
+    cats.remove(cat)
+    return cat, 204
+
+
+def find_by_id(uid):
+    try:
+        matching_cats = []
+        for c in cats:
+            if c["id"] == uid:
+                matching_cats.append(c)
+        return matching_cats[0]
+        # return next(c for c in cats if c["id"] == uid)
+    except:
+        raise exceptions.NotFound(f"We don't have a cat with that {uid} id!")
